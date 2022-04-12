f5a96a89c329d8bd2434aee0f81b168dce9b00cd
Gérard Dethier <gerard@logion.network>
2022-04-12 13:06:33 +0200

    feat: log mail when mailer disabled.
    logion-network/logion-internal#423

--- src/Mailer.ts
+++ src/Mailer.ts
@@ -1,6 +1,9 @@
 import { createTransport } from "nodemailer";
 import { Options as TransportOptions } from "nodemailer/lib/smtp-connection";
 import Mail, { Attachment } from "nodemailer/lib/mailer";
+import { getLogger } from "./util/Log";
+
+const logger = getLogger();
 
 export interface MailMessage {
     to: string;
@@ -24,6 +27,13 @@ export class Mailer {
 
     async sendMail(message: MailMessage) {
         if(!this.configuration.enabled) {
+            logger.info(`[Mailer disabled] Subject: ${message.subject}`);
+            logger.info(`[Mailer disabled] Body: ${message.text}`);
+            if(message.attachments) {
+                for(let i = 0; i < message.attachments.length; ++i) {
+                    logger.info(`[Mailer disabled] Attachment[${i}]: ${message.attachments[i].path}`);
+                }
+            }
             return;
         }
         const transport = createTransport(this.configuration);
